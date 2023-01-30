d6d7f5a442415a3c6a5f48fc9e616a8811de3dd6
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-01-31 00:50:37 +0900

    [create]画面に文字を表示

---
+++ php_practice1/sample01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sample01</title>
+</head>
+<body>
+  <?php
+  echo 'PHPを勉強中です';
+  ?>  
+</body>
+</html>
+
+<!-- echo 画面に何かを表示しますよという命令、今回ではファンクションと呼ぶ-->
+<!-- 'PHPを勉強中です'は、パラメーター(引数) echoに対しての補足情報である-->
+<!-- ファンクションは基本()で囲むが、echoは()が使えない -->
