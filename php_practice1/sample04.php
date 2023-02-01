31b770ca00c1a2c11764c860737d363815ed32a4
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-02-01 14:46:17 +0900

    [update]現在の日付・時刻を表示

---
+++ php_practice1/sample04.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+echo date('G時 i分 s秒');
+
+
+
+
+// dateもファンクションの一つ
+// echoと一緒にしか使えない
+// 表示された時刻の時差を防ぐためにdate_default_timezone_set('Asia/Tokyo');を記載
