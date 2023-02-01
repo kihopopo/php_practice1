f2c23e02a234846e550bdd435cf046201d001257
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-02-01 15:02:24 +0900

    [update]文字列と数式を連結

--- php_practice1/sample04.php
+++ php_practice1/sample04.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-echo date('G時 i分 s秒');
+echo '現在は、'.date('G時 i分 s秒').'です';
 
 
 
@@ -8,3 +8,13 @@ echo date('G時 i分 s秒');
 // dateもファンクションの一つ
 // echoと一緒にしか使えない
 // 表示された時刻の時差を防ぐためにdate_default_timezone_set('Asia/Tokyo');を記載
+
+// NG echo '現在は、date('G時 i分 s秒')です';
+// 「現在は」と「です」は文字列でシングルクォーテーションが必要となり、date()とそのまま表示されてしまう
+
+// echo '現在は、';
+// echo date('G時 i分 s秒');
+// echo 'です';
+// とすれば、表示されるがまとまりが悪い
+
+// 上の記述のように.（ドット）で繋げば文字列連結ができる
