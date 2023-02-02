a821ded91de6341639d682c89275234e355e7bad
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-02-03 01:23:21 +0900

    [create]変数を使った計算

--- php_practice1/sample05.php
+++ php_practice1/sample05.php
@@ -3,3 +3,8 @@ $today = new DateTime();
 $today->setTimezone(new DateTimeZone('Asia/Tokyo'));
 echo '現在は、' .$today->format('G時 i分 s秒').'です';
 ?>
+
+<!-- $todayは、オブジェクトの一種である
+dateと$today->formatはほぼ同じ意味
+オブジェクトを使う場合は、２行目のように宣言が必要
+オブジェクトとメソッドを繋ぐ場合、->が必要である -->
