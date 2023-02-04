3d202bb13902b5cdee724a6472ee2afc9e6d50a8
kihopopo <104831865+kihopopo@users.noreply.github.com>
2023-02-04 23:10:22 +0900

    [update]繰り返し処理するwhile構文2

--- php_practice1/sample07.php
+++ php_practice1/sample07.php
@@ -1,10 +1,8 @@
 <?php
 $i = 1;
-echo $i . '<br>';
-
 while ($i <366):
+    echo $i . '日<br>';
     $i = $i + 1;
-    echo $i . '<br>';
 endwhile;
 ?>
 
@@ -13,3 +11,4 @@ endwhile;
 <!-- $iは、ものを管理したり数えたりするもの -->
 <!-- 繰り返しは、whileを使う、()の中は条件を記載-->
 <!-- endwhileは、whileに戻る -->
+<!-- 
